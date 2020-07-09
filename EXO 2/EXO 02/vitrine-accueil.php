febb2505538a3bcebc312bbb7d7a925c9713e473
nathanportier <nportiern@gmail.com>
2020-07-09 18:24:33 +0200

    Create vitrine-accueil.php

---
+++ EXO 2/EXO 02/vitrine-accueil.php
@@ -0,0 +1,28 @@
+  <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Vitrine Summer Code Camp</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/vitrine.css">
+</head>
+
+<body>
+    <?php
+    $page_title = "Accueil";
+    include ("header.php")
+    ?>
+        <main>
+            <h2>Accueil</h2>
+            <div class="container">
+                
+                <p>Le numérique, nouvelle ère de l’humanité, source d’innovation et de performance ? Pour mieux comprendre cette révolution, venez apprendre les rudiments du code et découvrir les bases de la programmation des sites internet. Destiné aux étudiants, ce code camp vous initiera en un mois aux principaux langages de programmation Web (HTML-CSS et PHP), vous dévoilera le fonctionnement d’outils comme Google ou Paypal et vous éclaira sur la sécurité des sites internet.</p>
+            </div>
+            <div class="photo">
+                <img src="code.jpg" height="200px" width="300px">
+                <img src="net.jpg" height="200px" width="300px">
+            </div>
+        </main>
+        <?php include ("footer.php") ?>
+    </body>
+</html>
