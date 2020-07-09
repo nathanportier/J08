3cabbd5bf75669ded482b5eb3ad14ab3e210b8a6
nathanportier <nportiern@gmail.com>
2020-07-09 18:24:50 +0200

    Create 404.php

---
+++ EXO 2/EXO 02/404.php
@@ -0,0 +1,15 @@
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/vitrine.css">
+</head>
+<body>
+   
+        <main>
+           <img class=img_404 src="404.png" height="317px" width="560px">
+        </main>
+        
+</body>
+</html>
