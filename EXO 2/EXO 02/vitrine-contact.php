9c85be3a0db1e03adbecf53df84840ff10eaeebd
nathanportier <nportiern@gmail.com>
2020-07-09 18:24:36 +0200

    Create vitrine-contact.php

---
+++ EXO 2/EXO 02/vitrine-contact.php
@@ -0,0 +1,56 @@
+[13:49] Nathan Portier (Invité)
+    
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contacts</title>
+<metacharset="UTF-8">
+<linkrel="stylesheet"href="Style/vitrine.css">
+</head>
+ 
+<body>
+<header>
+<nav>
+<ahref="vitrine-accueil.php">ACCUEIL</a>
+<ahref="vitrine-programme.php">PROGRAMME</a>
+<ahref="vitrine-contacts.php">CONTACT</a>
+</nav>
+<h1>Summer Code Camp</h1>
+ 
+</header>
+ 
+<mainclass="CONTACT">
+ 
+<h2>CONTACT</h2>
+<mainclass="cadre">
+<formaction="/ma-page-de-traitement"method="post">
+<div>
+<inputtype="text"id="name"name="user_name"placeholder="Votre Nom">
+</div>
+<div>
+<br>
+<inputtype="email"id="mail"name="user_mail"placeholder="Votre e-mail">
+</div>
+<br>
+<div>
+<textareaid="msg"name="user_message"placeholder="Comment améliorer mon site ?"></textarea>
+</div>
+<br>
+<divclass="button">
+<buttontype="submit">Envoyer</button>
+</div>
+ 
+</form>
+</main>
+
+</main>
+
+<footer>
+<ahref="http://www.epitech.eu"><imgsrc=logo_epitech.png></a>
+
+
+</footer>
+</body>
+</html>
+
