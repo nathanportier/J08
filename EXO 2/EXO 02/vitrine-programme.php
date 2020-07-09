b26a653e3a62113a1747efbb223563393d2dc7ac
nathanportier <nportiern@gmail.com>
2020-07-09 18:24:25 +0200

    Create vitrine-programme.php

---
+++ EXO 2/EXO 02/vitrine-programme.php
@@ -0,0 +1,122 @@
+
+    
+
+<!DOCTYPE html>
+<html>
+<head>
+<h1>Programme</h1>
+<metacharset="UTF-8">
+<linkrel="stylesheet"href="Style/vitrine.css">
+</head>
+ 
+<body>
+<header>
+<nav>
+<h1>Summer Code Camp</h1>
+ 
+</header>
+ 
+<mainclass="PROGRAMME">
+<h2>Programme ISCC 2020</h2>
+
+<table>
+<trclass="Un">
+<th>DATE</th>
+<tdclass="center">PROGRAMME</td>
+</tr>
+<tr>
+<thscope="col">Jour 1</th>
+<td>Accueil des apprenants Installation et Conférence</td>
+</tr>
+<tr>
+<thscope="row">Jour 2</th>
+<td>HTML-1</td>
+</tr>
+<tr>
+<thscope="row">Jour 3</th>
+<td>HTML-2</td>
+</tr>
+<tr>
+<thscope="row">Jour 4</th>
+<td>HTML-3 et Conférence</td>
+</tr>
+<tr>
+<thscope="row">Jour 5</th>
+<td>PHP-1</td>
+</tr>
+<tr>
+<thscope="row">&nbsp</th>
+<td>&nbsp</td>
+</tr>
+<tr>
+<thscope="row">&nbsp</th>
+<td>&nbsp</td>
+</tr>
+<tr>
+<thscope="row">Jour 6</th>
+<td>PHP-2</td>
+</tr>
+<tr>
+<thscope="row">Jour 7</th>
+<td>PHP-3</td>
+</tr>
+<tr>
+<thscope="row">Jour 8</th>
+<td>HPHP-4</td>
+</tr>
+<tr>
+<thscope="row">Jour 9</th>
+<td>PHP-5</td>
+</tr>
+<tr>
+<thscope="row">Jour 10</th>
+<td>SQL-1</td>
+</tr>
+<tr>
+<thscope="row">&nbsp</th>
+<td>&nbsp</td>
+</tr>
+<tr>
+<thscope="row">&nbsp</th>
+<td>&nbsp</td>
+</tr>
+<tr>
+<thscope="row">Jour 11</th>
+<td>SQL-2</td>
+</tr>
+<tr>
+<thscope="row">Jour 12</th>
+<td>SQL-3 et Conférence</td>
+</tr>
+<tr>
+<thscope="row">Jour 13</th>
+<td>Projet MyDevBlog</td>
+</tr>
+<tr>
+<thscope="row">Jour 14</th>
+<td>Projet MyDevBlog et Conférence</td>
+</tr>
+<tr>
+<thscope="row">Jour 15</th>
+<td>Projet MyDevBlog</td>
+</tr>
+<tr>
+<thscope="row">&nbsp</th>
+<td>&nbsp</td>
+</tr>
+<tr>
+<thscope="row">Jour 16</th>
+<td>Soutenance Projet MyDevBlog</td>
+</tr>
+</table>
+ 
+</main>
+
+<footer>
+<ahref="http://www.epitech.eu"><imgsrc=logo_epitech.png></a>
+
+
+</footer>
+</body>
+</html>
+
