b805e32cd9e5cdfb337a241221bf5eda3ed99b6d
nathanportier <nportiern@gmail.com>
2020-07-09 18:24:39 +0200

    Create Nav.php

---
+++ EXO 2/EXO 02/Nav.php
@@ -0,0 +1,9 @@
+<nav>
+            <ul class="vertical-list centered">
+
+                <a <?php if($page_title == "Accueil") { echo 'class="active"';} ?> href="vitrine-accueil.php"> Accueil</a>
+                <a <?php if($page_title == "Programme") { echo 'class="active"';} ?>href="vitrine-programme.php">programme</a>
+                <a <?php if($page_title == "Contact") { echo 'class="active"';} ?>href="vitrine-contact.php">Contact</a>
+            </ul>
+        </nav>
+            
